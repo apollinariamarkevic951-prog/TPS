2b41cd0679dea3c546a27ca84aa2357e16478386
Маркевич Аполлинария <apollinariamarkevic951@gmail.com>
2026-02-25 15:16:40 +1000

    ldjffl

--- sql/init.sql
+++ sql/init.sql
@@ -1,26 +1,31 @@
 CREATE TABLE IF NOT EXISTS videos (
-  id BIGINT PRIMARY KEY,
-  creator_id BIGINT NOT NULL,
+  id TEXT PRIMARY KEY,
+  creator_id TEXT NOT NULL,
   video_created_at TIMESTAMP NOT NULL,
+
   views_count BIGINT NOT NULL,
   likes_count BIGINT NOT NULL,
   comments_count BIGINT NOT NULL,
   reports_count BIGINT NOT NULL,
+
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS video_snapshots (
-  id BIGINT PRIMARY KEY,
-  video_id BIGINT NOT NULL REFERENCES videos(id) ON DELETE CASCADE,
+  id TEXT PRIMARY KEY,
+  video_id TEXT NOT NULL REFERENCES videos(id) ON DELETE CASCADE,
+
   views_count BIGINT NOT NULL,
   likes_count BIGINT NOT NULL,
   comments_count BIGINT NOT NULL,
   reports_count BIGINT NOT NULL,
+
   delta_views_count BIGINT NOT NULL,
   delta_likes_count BIGINT NOT NULL,
   delta_comments_count BIGINT NOT NULL,
   delta_reports_count BIGINT NOT NULL,
+
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 );
